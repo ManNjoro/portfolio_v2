5303695c6ccb5fc1680f53bf3e3a856606fb6186
ManNjoro <elijohnmwoho@gmail.com>
2025-05-12 16:52:09 +0300

    Added project logos and updated project data in projects.tsx

--- src/api/projects.tsx
+++ src/api/projects.tsx
@@ -1,4 +1,5 @@
 import { nanoid } from "nanoid";
+import { federated,mkulima,zoomeek } from "@/logos";
 
 type Project = {
     id: string;
@@ -6,16 +7,34 @@ type Project = {
     description: string;
     image: string;
     url: string;
+    technologies: Array<string>;
 }
 
 const projects: Project[] = [
     {
         id: nanoid(),
-        title: "Project 1",
-        description: "This is a description of project 1",
-        image: "https://picsum.photos/200/300",
-        url: "https://example.com/project1",
-    }
+        title: "Zoommeek",
+        description: "A tech company that offers solutions including internet, security and gas",
+        image: zoomeek,
+        url: "https://zoommeekafrica.co.ke",
+        technologies: ['react','javascript', 'tailwind', 'django', 'mysql']
+    },
+    {
+        id: nanoid(),
+        title: "Diabetes Detector",
+        description: "A privacy-preserving AI system for early diabetes prediction using distributed machine learning across healthcare institutions.",
+        image: federated,
+        url: "https://github.com/ManNjoro/federated_learning",
+        technologies: ['react','javascript', 'tailwind','python', 'flask', 'flower']
+    },
+    {
+        id: nanoid(),
+        title: "M-kulima",
+        description: "A Farm Management Mobile Application designed to help farmers track and manage their agricultural operations",
+        image: mkulima,
+        url: "https://github.com/ManNjoro/federated_learning",
+        technologies: ['react native','javascript', 'expo','sqlite']
+    },
 ]
 
 export default projects;
--- src/logos.ts
+++ src/logos.ts
@@ -8,14 +8,22 @@ import tailwind from './assets/tailwind.png'
 import typescript from './assets/typescript.png'
 import vite from './assets/vite.png'
 
+// project images
+import zoomeek from './assets/zoomeek.png'
+import federated from './assets/federated.png'
+import mkulima from './assets/mkulima.jpeg'
+
 export {
     django,
+    federated,
     javascript,
+    mkulima,
     mysql,
     nodejs,
     python,
     reactjs,
     tailwind,
     typescript,
-    vite
+    vite,
+    zoomeek
 }
